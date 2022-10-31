a6bec92eafa2d2fea73afa11e6dd19cdb48ad33a
Epileptick1dZ <117070345+Epileptick1dZ@users.noreply.github.com>
2022-10-31 13:43:56 +0300

    Test
    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ using System;
+
+namespace TMP_LAB9_precommit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
